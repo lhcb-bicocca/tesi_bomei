b707b668085b9db7df021b2593107ff0e326f9d9
ale-bomei <a.bomei@campus.unimib.it>
2026-09-03 00:50:02 +0200

    Creata funzione compute Cls

---
+++ macros/test_funzione.py
@@ -0,0 +1,67 @@
+from mypkg.CLs_library import compute_cls
+import pyhf 
+
+s = 100 	   #segnale atteso
+b = 1000 	   #background atteso
+n_obs = 1050    #eventi osservati
+mu_test = 1    #Supponiamo che segnale esista
+	
+spec = {
+    "channels": [
+        {
+            "name": "single_channel",
+            "samples": [
+                {
+                    "name": "background",
+                    "data": [b],
+                    "modifiers": []
+                },
+                {
+                    "name": "signal",
+                    "data": [s],
+                    "modifiers": [
+                        {"name": "mu", "type": "normfactor", "data": None}
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
+model = pyhf.Model(spec, poi_name = "mu")	
+
+compute_cls(
+    calculator="toys",
+    n_obs=n_obs,
+    b=b,
+    s=s,
+    mu=mu_test,
+    PRINT=True
+)
+
+compute_cls(
+    calculator="poisson",
+    n_obs=n_obs,
+    b=b,
+    s=s,
+    mu=mu_test,
+    PRINT=True
+)
+
+compute_cls(
+    calculator="pyhf",
+    model = model,
+    n_obs=n_obs,
+    mu=mu_test,
+    calctype = "toys",
+    PRINT=True
+)
+
+compute_cls(
+    calculator="pyhf",
+    model = model,
+    n_obs=n_obs,
+    mu=mu_test,
+    calctype = "asymptotics",
+    PRINT=True
+)
