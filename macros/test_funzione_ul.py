91c203b9c12d55a464ebeed5d7f8a1f3b1ac2c1f
ale-bomei <a.bomei@campus.unimib.it>
2026-09-13 21:09:23 +0200

    UL expected

--- macros/test_funzione_ul.py
+++ macros/test_funzione_ul.py
@@ -7,9 +7,10 @@ b = 10 	   #background atteso
 n_obs = 8    #eventi osservati
 mu_test = 1    #Supponiamo che segnale esista
 
-mu_up = upper_limit(n_obs, b, s, calculator = "asymptotics")
+mu_up_obs, mu_up_exp = upper_limit(n_obs, b, s, calculator = "asymptotics")
 
-print(f"95% CL upper limit: mu < {mu_up:.4f}")
+print(mu_up_obs)
+print(mu_up_exp)
 
 spec = {
     "channels": [
@@ -25,21 +26,32 @@ spec = {
                     "name": "signal",
                     "data": [s],
                     "modifiers": [
-                        {"name": "mu", "type": "normfactor", "data": None}
+                        {
+                            "name": "mu",
+                            "type": "normfactor",
+                            "data": None
+                        }
                     ]
                 }
             ]
+        }
+    ],
+    "parameters": [
+        {
+            "name": "mu",
+            "inits": [1.0],
+            "bounds": [[0.0, 30.0]]
         }
     ]
 }
 
 model = pyhf.Model(
     spec,
-    poi_name="mu",
+    poi_name="mu"
 )
 
 #Calcolo upper limit
-scan = np.linspace(0, 10, 100)
+scan = np.linspace(0, 20, 201)
 obs_limit, exp_limits, (scan, results) = pyhf.infer.intervals.upper_limits.upper_limit(
     [n_obs], model, scan = scan, level = 0.05, return_results=True
 )
